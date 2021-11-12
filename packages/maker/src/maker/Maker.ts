b53e22f2ffb1b311233eb9a95d27b9e9e93ee438
CheshireCatNick <CheshireCatNick@gmail.com>
2021-11-12 20:21:12 +0800

    feat(Maker): remove log bp

--- packages/maker/src/maker/Maker.ts
+++ packages/maker/src/maker/Maker.ts
@@ -90,7 +90,6 @@ export class Maker extends BotService {
             // TODO: use Promise.all()
             for (const market of Object.values(this.marketMap)) {
                 const buyingPower = await this.perpService.getBuyingPower(this.wallet.address)
-                console.log(+buyingPower)
                 try {
                     await this.refreshCurrentRangeOrders(market)
                     await this.adjustCurrentRangeLiquidity(market)
